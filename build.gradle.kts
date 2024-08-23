4e7a8d23c1011c0203922d53201e5e297df4c86d
CptBeffHeart <cptbeffheart@gmail.com>
2024-08-23 16:49:39 +0200

    refactor: update gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,8 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 group = "com.expectale"
 version = "2.0.0"
-
-val mojangMapped = project.hasProperty("mojang-mapped")
 
 plugins {
     alias(libs.plugins.kotlin)
@@ -20,7 +19,7 @@ repositories {
 dependencies {
     paperweight.paperDevBundle(libs.versions.paper)
     implementation(libs.nova)
-    compileOnly("xyz.xenondevs.nova:nova-api:0.16")
+    compileOnly("xyz.xenondevs.nova:nova-api:0.17-alpha.11")
 }
 
 addon {
@@ -35,22 +34,21 @@ addon {
 tasks {
     register<Copy>("addonJar") {
         group = "build"
-        if (mojangMapped) {
-            dependsOn("jar")
-            from(File(buildDir, "libs/${project.name}-${project.version}-dev.jar"))
-        } else {
-            dependsOn("reobfJar")
-            from(File(buildDir, "libs/${project.name}-${project.version}.jar"))
-        }
-        
-        from(File(project.buildDir, "libs/${project.name}-${project.version}.jar"))
-        into((project.findProperty("outDir") as? String)?.let(::File) ?: project.buildDir)
+        dependsOn("jar")
+        from(File(project.layout.buildDirectory.get().asFile, "libs/${project.name}-${project.version}.jar"))
+        into((project.findProperty("outDir") as? String)?.let(::File) ?: project.layout.buildDirectory.get().asFile)
         rename { "${addonMetadata.get().addonName.get()}-${project.version}.jar" }
     }
     
     withType<KotlinCompile> {
-        kotlinOptions {
-            jvmTarget = "17"
+        compilerOptions {
+            jvmTarget = JvmTarget.JVM_21
         }
+    }
+}
+
+afterEvaluate {
+    tasks.getByName<Jar>("jar") {
+        archiveClassifier = ""
     }
 }
